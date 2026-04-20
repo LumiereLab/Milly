28a8f0ceef1cb2b5e2ecc40ea8d6ae3e829e643d
Moonlitwalk <sendmeforthmoonlitwalk@gmail.com>
2026-04-20 04:55:43 +0200

    localstorage

--- milly-backend/src/app.controller.ts
+++ milly-backend/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('tickets')
+  getTIckets() {
+    return this.appService.getTickets();
+  }
 }
--- milly-backend/src/app.service.ts
+++ milly-backend/src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getTickets() {
+    return [
+      { id: 1, title: 'first backend', status: 'open' },
+      { id: 2, title: 'second backend', status: 'progressed' },
+    ];
+  }
 }
