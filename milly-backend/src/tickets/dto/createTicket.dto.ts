6151f6f5b134dd12f3e2f47dcb6fc131be6dfcab
Moonlitwalk <sendmeforthmoonlitwalk@gmail.com>
2026-05-03 20:24:18 +0200

    added title and status conditions for request handling

--- milly-backend/src/tickets/dto/createTicket.dto.ts
+++ milly-backend/src/tickets/dto/createTicket.dto.ts
@@ -1,7 +1,11 @@
-import { isString, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateTicketDto {
-  @IsString()
+  
+  @IsString( {message: 'Title must be a string!'} )
+  @IsNotEmpty( {message:'Title cannot be empty!'} )
   title: string;
-  @IsString()
+
+  @IsString( {message: 'Status must be a string!'} )
+  @IsNotEmpty({ message: 'Status cannot be empty!'})
   status: string;
 }
